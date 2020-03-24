69120fcf5adb9d457eaaf20e6c1ddba52628fff0
teacher <net94.teacher@gmail.com>
2020-03-24 15:20:51 +0900

    affd

--- WebContent/jQuery/chap04/04_01_json/scripts.js
+++ WebContent/jQuery/chap04/04_01_json/scripts.js
@@ -1,47 +1,22 @@
 $(function(){
-
-	// tbody요소를 취득
 	var tbody = $('#tbody');
-
-	// data.xml을 가져 오기
-	$.ajax({
-		url: 'data.xml',
-		dataType: 'xml',
-		success: function(xml){
+	$.getJSON('http://192.168.137.1:8080/spring5_mvc1_study/api/members', function(people){
+		$.each(people, function(i, person){
+			var text_no = person.id;
+			var text_name = person.name;
+			var text_mail = person.email;
+			var text_regDate = person.registerDateTime;
 			
-			// xml도큐먼트를 jQuery객체화
-			var people = $(xml);
-
-			// 안에 있는 person요소 각각에 대해서 처리하기
-			people.find('person').each(function(){
-
-				// person요소를 jQuery객체화
-				var person = $(this);
-
-				// 각각의 자식요소를 취득하여, 텍스트를 취득
-				var text_no = person.find('no').text();
-				var text_name = person.find('name').text();
-				var text_mail = person.find('mail').text();
-
-				// tr요소를 만들기
-				var tr = $('<tr />');
-
-				// td요소를 만들기
-				var no = $('<td />').text(text_no);
-				var name = $('<td />').text(text_name);
-				var mail = $('<td />').text(text_mail);
-
-				// tr요소에 td요소를 추가
-				tr.append(no);
-				tr.append(name);
-				tr.append(mail);
-
-				// tbody에 tr요소를 append
-				tbody.append(tr);
-
-			});
-
-		}
+			var tr = $('<tr />');
+			var no = $('<td />').text(text_no);
+			var name = $('<td />').text(text_name);
+			var mail = $('<td />').text(text_mail);
+			var reg = $('<td/>').text(text_regDate);
+			tr.append(no);
+			tr.append(name);
+			tr.append(mail);
+			tr.append(reg);
+			tbody.append(tr);
+		});
 	});
-
 });
